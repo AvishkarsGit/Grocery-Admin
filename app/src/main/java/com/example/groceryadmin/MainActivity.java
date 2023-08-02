4d9354271ffbf2352f1f5c8bf13aa0d0855cb74f
Avishkar Kumbhar <kumbharavishkar7@gmail.com>
2023-08-02 21:10:22 +0530

    update code

--- app/src/main/java/com/example/groceryadmin/MainActivity.java
+++ app/src/main/java/com/example/groceryadmin/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.groceryadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button avishkar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        avishkar = findViewById(R.id.avishkar);
+
+        avishkar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This from avishkar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
