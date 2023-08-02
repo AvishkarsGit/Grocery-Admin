7d52ff334786e893cb73312f21f2cb5748c9035f
shambho ramchandra jaybhaye <92688314+Shambhogit@users.noreply.github.com>
2023-08-02 21:07:17 +0530

    toast commit

--- app/src/main/java/com/example/groceryadmin/MainActivity.java
+++ app/src/main/java/com/example/groceryadmin/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.groceryadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnShambho;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShambho = findViewById(R.id.btnShambho);
+        
+        btnShambho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This is toast from Shambho", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
