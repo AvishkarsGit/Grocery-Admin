d6e2cdfe6ae10f806aeedcf92f773d3ef1f80fb1
Avishkar Kumbhar <kumbharavishkar7@gmail.com>
2023-08-03 13:00:55 +0530

    just thing

--- app/src/main/java/com/example/groceryadmin/LoginActivity.java
+++ app/src/main/java/com/example/groceryadmin/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.groceryadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
